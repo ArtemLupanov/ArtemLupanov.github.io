c2a7342c93b5f62854c77dc2e66257ab803a6925
ArtemLupanov <84403157+ArtemLupanov@users.noreply.github.com>
2021-07-12 20:52:39 +0300

    prompt и alert
    Добавил вопросы
    Без продумывания логики, если нажали другую цифру, букву или ничего не ввели

--- src/js/script.js
+++ src/js/script.js
@@ -18,3 +18,40 @@ if (iconMenu) {
    })
 
 }
+
+setTimeout(function () {
+   let type = prompt("Тип сайта: 1 - Одностраничный, 2 - магазин");
+   let dayType;
+   if (type === "1") {
+      type = 2000;
+      dayType = 1;
+   } else {
+      type = 10000;
+      dayType = 7;
+   };
+
+   let design = prompt("Дизайн: 1 - Делаю я, 2 - Делаете вы");
+   let dayDesign;
+   if (design === "1") {
+      design = 2000;
+      dayDesign = 2;
+   } else {
+      design = 0;
+      dayDesign = 0;
+   };
+
+   let adaptability = prompt("Адаптивность: 1 - Есть, 2 - Нет");
+   let dayAdaptability;
+   if (adaptability === "1") {
+      adaptability = 5000;
+      dayAdaptability = 2;
+   } else {
+      adaptability = 1000;
+      dayAdaptability = 1;
+   };
+
+   let price = type + design + adaptability;
+   let sumDay = dayType + dayDesign + dayAdaptability;
+
+   alert(`Итоговая цена = ${price}₽. Дней будет затрачено = ${sumDay}`);
+}, 2000);
